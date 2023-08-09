86871c91c2ebbd1d5bab595afe1c114d12a191dc
Franco Martinez <franco.mcarballo@outlook.com>
2023-08-09 16:30:56 -0600

    using refs and reactive

---
+++ src/views/HomeView-Patterns.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="home">
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+<!-- Setup Function Pattern -->
+<!--
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const counter = ref(0)
+    const increaseCounter = () => {
+      counter.value++
+    }
+    const decreaseCounter = () => {
+      counter.value--
+    }
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter
+    }
+  }
+}
+</script>
+-->
+
+<!-- Setup Function Pattern -->
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+const increaseCounter = () => {
+  counter.value++
+}
+
+const decreaseCounter = () => {
+  counter.value--
+}
+</script>
+
+<!-- options API -->
+<!-- <script>
+// export default {
+//   data() {
+//     return {
+//       counter: 0
+//     }
+//   },
+//   methods: {
+//     increaseCounter() {
+//       this.counter++
+//     },
+//     decreaseCounter() {
+//       this.counter--
+//     }
+//   }
+// }
+// </script>
+-->
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,40 @@
 <template>
   <div class="home">
+    <h3>{{ counterData.title }}</h3>
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit Counter Title:</h4>
+      <input v-model="counterData.title" type="text" />
     </div>
   </div>
 </template>
 
-<!-- Setup Function Pattern -->
-<!--
-<script>
-import { ref } from 'vue'
-export default {
-  setup() {
-    const counter = ref(0)
-    const increaseCounter = () => {
-      counter.value++
-    }
-    const decreaseCounter = () => {
-      counter.value--
-    }
-
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter
-    }
-  }
-}
-</script>
--->
-
-<!-- Setup Function Pattern -->
 <script setup>
-import { ref } from 'vue'
+// import { ref, reactive } from 'vue'
+import { reactive } from 'vue'
 
-const counter = ref(0)
+// const counter = ref(0)
+// const counterTitle = ref('just a Counter')
+
+const counterData = reactive({
+  count: 0,
+  title: 'the Counter'
+})
 
 const increaseCounter = () => {
-  counter.value++
+  counterData.count++
 }
 
 const decreaseCounter = () => {
-  counter.value--
+  counterData.count--
 }
 </script>
 
-<!-- options API -->
-<!-- <script>
-// export default {
-//   data() {
-//     return {
-//       counter: 0
-//     }
-//   },
-//   methods: {
-//     increaseCounter() {
-//       this.counter++
-//     },
-//     decreaseCounter() {
-//       this.counter--
-//     }
-//   }
-// }
-// </script>
--->
 <style>
 .home {
   text-align: center;
@@ -75,5 +44,8 @@ const decreaseCounter = () => {
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+.edit {
+  margin-top: 60px;
 }
 </style>
